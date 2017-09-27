431b288ee8a784468e98be695364f6ea1e1981ba
adam.svoboda <svoboda.adams@gmail.com>
2017-09-27 22:43:25 +0200

    First commit

---
+++ App_Code/Functions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Contains all needed functions
+/// </summary>
+public class Functions
+{
+    /// <summary>
+    /// Returns value of key from query. if there is no, return default
+    /// </summary>
+    public static string RQ(string name, string def)
+    {
+        return HttpContext.Current.Request.QueryString[name] ?? def;
+    }
+
+    /// <summary>
+    /// Returns value of key from form. if there is no, return default
+    /// </summary>
+    public static string RF(string name, string def)
+    {
+        return HttpContext.Current.Request.Form[name] ?? def;
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(Functions.RQ("ajax", "0") == "1")
+        {
+            Response.Write("Success");
+            Response.End();
+        }
+    }
+}
